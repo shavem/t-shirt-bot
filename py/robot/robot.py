ae06ee1ba2c3d04694cb6ec13599646e2251f7a3
Shavem <shivam.aarya2017@gmail.com>
2021-12-01 16:25:16 -0600

    not first push

--- py/robot/robot.py
+++ py/robot/robot.py
@@ -75,6 +75,7 @@ class SpartaBot(magicbot.MagicRobot):
 
     def teleopPeriodic(self):
 
+        # drivetrain
         # Solenoid test
         if self.drive_controller.getBButtonReleased():
             self.solenoid.set(self.shift_toggle)
